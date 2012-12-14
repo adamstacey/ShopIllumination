359198543aeb1f3a491b94a6447565f91f4e531d
Daniel Cannon <daniel@danielcannon.co.uk>
2012-12-14 22:19:30 +0000

    Fix left over code in deploy.rb

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -14,7 +14,6 @@ set :webserver_user,    "apache"
 set :permission_method, :chmod
 set :use_set_permissions, true
 set :use_sudo , false
->>>>>>> b6ae4eebedfe25f9e28ec556d7fb65bbaede62c9
 
 # Repository configuration
 set :repository,  "git@github.com:adamstacey/KitchenApplianceCentre.git"
@@ -57,4 +56,4 @@ namespace :deploy do
   end
 end
 
-logger.level = Logger::MAX_LEVEL
+#logger.level = Logger::MAX_LEVEL
