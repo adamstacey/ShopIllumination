8084a988cd78d9b69cb3d1a6f259727e0ed3dfa8
Daniel Cannon <daniel@danielcannon.co.uk>
2012-12-14 22:17:34 +0000

    Switched capifony to use master branch

--- app/config/dev.rb
+++
@@ -1,7 +0,0 @@
-server "127.0.0.1", :app, :web, :db, :primary => true
-
-set :domain,      "127.0.0.1"
-set :user, "vagrant"
-set :password, "vagrant"
-set :deploy_to,        "/var/www/application"
-ssh_options[:port] = 2222
--- app/config/staging.rb
+++ app/config/staging.rb
@@ -2,6 +2,5 @@ set :domain, "staging.kitchenappliancecentre.co.uk"
 set :user, "root"
 set :deploy_to, "/var/www/vhosts/#{domain}/webapps"
 ssh_options[:port] = 2020
-set :branch, "capifony-deploy-script"
 
 server "staging.kitchenappliancecentre.co.uk", :app, :web, :db, :primary => true
