2409343f1da0dae30f0e4e23a9ca0a0fe7d1bcfd
Daniel Cannon <daniel@danielcannon.co.uk>
2012-12-18 11:16:55 +0000

    Staging env now includes app_dev.php

--- app/config/staging.rb
+++
@@ -1,6 +0,0 @@
-set :domain, "staging.kitchenappliancecentre.co.uk"
-set :user, "root"
-set :deploy_to, "/var/www/vhosts/#{domain}/webapps"
-ssh_options[:port] = 2020
-
-server "staging.kitchenappliancecentre.co.uk", :app, :web, :db, :primary => true
