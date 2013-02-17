89ef2d87d744c917ccd0e718c599a17e5a4fd1ff
Adam Stacey <me@adamstacey.co.uk>
2013-02-17 17:54:41 +0000

    Updated the validation groups for pricing from step 1 to 3.

--- src/WebIllumination/SiteBundle/Entity/Product/Price.php
+++ src/WebIllumination/SiteBundle/Entity/Product/Price.php
@@ -31,22 +31,22 @@ class Price
     
     /**
      * @ORM\Column(name="cost_price", type="decimal", precision=12, scale=4)
-     * @Assert\NotBlank(groups={"flow_site_new_product_step1", "site_edit_product_prices"})
-     * @Assert\Min(limit="0", groups={"flow_site_new_product_step1", "site_edit_product_prices"})
+     * @Assert\NotBlank(groups={"flow_site_new_product_step3", "site_edit_product_prices"})
+     * @Assert\Min(limit="0", groups={"flow_site_new_product_step3", "site_edit_product_prices"})
      */
     private $costPrice = 0;
     
     /**
      * @ORM\Column(name="recommended_retail_price", type="decimal", precision=12, scale=4)
-     * @Assert\NotBlank(groups={"flow_site_new_product_step1", "site_edit_product_prices"})
-     * @Assert\Min(limit="0", groups={"flow_site_new_product_step1", "site_edit_product_prices"})
+     * @Assert\NotBlank(groups={"flow_site_new_product_step3", "site_edit_product_prices"})
+     * @Assert\Min(limit="0", groups={"flow_site_new_product_step3", "site_edit_product_prices"})
      */
     private $recommendedRetailPrice = 0;
     
     /**
      * @ORM\Column(name="list_price", type="decimal", precision=12, scale=4)
-     * @Assert\NotBlank(groups={"flow_site_new_product_step1", "site_edit_product_prices"})
-     * @Assert\Min(limit="0", groups={"flow_site_new_product_step1", "site_edit_product_prices"})
+     * @Assert\NotBlank(groups={"flow_site_new_product_step3", "site_edit_product_prices"})
+     * @Assert\Min(limit="0", groups={"flow_site_new_product_step3", "site_edit_product_prices"})
      */
     private $listPrice = 0;
     
@@ -57,7 +57,7 @@ class Price
     
     /**
      * @ORM\Column(name="display_order", type="integer", length=11, nullable=true)
-     * @Assert\NotBlank(groups={"flow_site_new_product_step1", "site_edit_product_prices"})
+     * @Assert\NotBlank(groups={"site_edit_product_prices"})
      */
     private $displayOrder = 1;
 
