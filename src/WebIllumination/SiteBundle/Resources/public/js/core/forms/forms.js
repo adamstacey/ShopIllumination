dc6dc7297c4fbdd3b189d7178fedee1bddf21a6f
Adam Stacey <me@adamstacey.co.uk>
2013-02-17 22:59:16 +0000

    Added a function to generate and re-build the form elements for any given html object.

--- src/WebIllumination/SiteBundle/Resources/public/js/core/forms/forms.js
+++ src/WebIllumination/SiteBundle/Resources/public/js/core/forms/forms.js
@@ -1,3 +1,26 @@
+function generateFormElements($object) {
+    $object.find("input, textarea, select, button, a.button").not(".no-uniform").uniform();
+}
+
 $(document).ready(function() {
-    $("input, textarea, select, button, a.button").not(".no-uniform").uniform();
+    generateFormElements($(document));
+
+    $(".actionAddFormRow").click(function() {
+        var $tableObject = $("#"+$(this).attr("data-table-object"));
+        var $rowCount = $tableObject.find("tr").length + 1;
+        var $newRow = $tableObject.attr("data-prototype");
+        $newRow = $newRow.replace(/__name__/g, $rowCount);
+        $newRow = $("<tr></tr>").html($newRow);
+        $newRow.appendTo($tableObject.find("tbody"));
+        generateFormElements($newRow);
+        generateButtons($newRow);
+        return false;
+    });
+
+    $(document).on("click", ".actionDeleteFormRow", function() {
+        var $tableObject = $("#"+$(this).attr("data-table-object"));
+        if ($tableObject.find("tbody tr").length > 1) {
+            $(this).closest("tr").remove();
+        }
+    });
 });
