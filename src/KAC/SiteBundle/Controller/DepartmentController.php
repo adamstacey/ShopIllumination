8fe232ddbe2ed344327fe0f073f9eef4f452cbb8
Adam Stacey <me@adamstacey.co.uk>
2013-03-08 23:33:11 +0000

    Added a new department controller and added an action to handle generating the main menu.

---
+++ src/KAC/SiteBundle/Controller/DepartmentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace KAC\SiteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * @Route("/")
+ */
+class DepartmentController extends Controller
+{
+    /**
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function mainMenuAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $departments = $em->getRepository("KACSiteBundle:Department")->findBy(array('lvl' => 1), array('displayOrder' => 'ASC'));
+
+        return array('departments' => $departments);
+    }
+}
