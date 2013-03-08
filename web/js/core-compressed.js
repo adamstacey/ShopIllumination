02e5c5a9e0569537516c40c85f39149fdaf664d9
Adam Stacey <me@adamstacey.co.uk>
2013-03-08 23:33:51 +0000

    Added auto-generated CSS and JS files.

--- web/js/core-compressed.js
+++ web/js/core-compressed.js
@@ -8601,3 +8601,19 @@ $(document).ready(function() {
         }
     });
 });
+$(document).ready(function() {
+    $(document).on("click focus blur keypress keyup keydown", ".search-container input", function() {
+        if ($(this).val() == "")
+        {
+            $(".search-container .icon-dark-red").hide();
+        } else {
+            $(".search-container .icon-dark-red").show();
+        }
+    });
+
+    $(document).on("click", ".search-container .icon-dark-red", function() {
+        $(".search-container input").val("");
+        $(this).hide();
+        $(".search-container input").focus();
+    });
+});
