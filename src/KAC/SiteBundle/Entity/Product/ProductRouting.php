f3cd2d3669de8a37e4cffe026bb11700ecc658a0
Adam Stacey <me@adamstacey.co.uk>
2013-03-12 14:02:45 +0000

    Added the product routing entity.

---
+++ src/KAC/SiteBundle/Entity/Product/ProductRouting.php
@@ -0,0 +1,41 @@
+<?php
+namespace KAC\SiteBundle\Entity\Product;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use KAC\SiteBundle\Entity\Routing;
+
+/**
+ * @ORM\Entity
+ */
+class ProductRouting extends Routing
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="KAC\SiteBundle\Entity\Product")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id")
+     */
+    protected $product;
+
+    /**
+     * Set product
+     *
+     * @param \KAC\SiteBundle\Entity\Product $product
+     * @return ProductRouting
+     */
+    public function setProduct(\KAC\SiteBundle\Entity\Product $product = null)
+    {
+        $this->product = $product;
+    
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return \KAC\SiteBundle\Entity\Product 
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+}
