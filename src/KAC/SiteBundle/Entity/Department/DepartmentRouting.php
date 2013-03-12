f05a912696e193bb9c7ebc9670fce6cb928e9261
Adam Stacey <me@adamstacey.co.uk>
2013-03-12 14:03:07 +0000

    Added the department routing entity.

---
+++ src/KAC/SiteBundle/Entity/Department/DepartmentRouting.php
@@ -0,0 +1,41 @@
+<?php
+namespace KAC\SiteBundle\Entity\Department;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use KAC\SiteBundle\Entity\Routing;
+
+/**
+ * @ORM\Entity
+ */
+class DepartmentRouting extends Routing
+{
+    /**
+     * @ORM\ManyToOne(targetEntity="KAC\SiteBundle\Entity\Department")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id")
+     */
+    protected $department;
+
+    /**
+     * Set department
+     *
+     * @param \KAC\SiteBundle\Entity\Department $department
+     * @return DepartmentRouting
+     */
+    public function setDepartment(\KAC\SiteBundle\Entity\Department $department = null)
+    {
+        $this->department = $department;
+    
+        return $this;
+    }
+
+    /**
+     * Get department
+     *
+     * @return \KAC\SiteBundle\Entity\Department
+     */
+    public function getDepartment()
+    {
+        return $this->department;
+    }
+}
