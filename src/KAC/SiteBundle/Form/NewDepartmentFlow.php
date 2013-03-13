23435062535d02384c2e410d153194d3627fb20b
Adam Stacey <me@adamstacey.co.uk>
2013-03-13 14:24:50 +0000

    Added new department flow based on the new product flow.

---
+++ src/KAC/SiteBundle/Form/NewDepartmentFlow.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace KAC\SiteBundle\Form;
+
+use Craue\FormFlowBundle\Event\PostBindRequestEvent;
+use Craue\FormFlowBundle\Event\PostBindSavedDataEvent;
+use Craue\FormFlowBundle\Event\PostValidateEvent;
+use Craue\FormFlowBundle\Event\PreBindEvent;
+use Craue\FormFlowBundle\Form\FormFlowEvents;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+use Craue\FormFlowBundle\Form\FormFlow;
+use KAC\SiteBundle\Entity\Product\Variant;
+use KAC\SiteBundle\Entity\Product\VariantToFeature;
+use KAC\SiteBundle\Entity\Product\Price;
+use KAC\SiteBundle\Entity\Product\Variant\Description;
+use KAC\SiteBundle\Entity\Product;
+
+class NewDepartmentFlow extends FormFlow
+{
+    protected $maxSteps = 6;
+    protected $allowDynamicStepNavigation = true;
+
+    protected function loadStepDescriptions() {
+        return array(
+            '1. Overview',
+            '2. Build Combinations',
+            '3. Prices',
+            '4. Features',
+            '5. Images',
+            '6. Links'
+        );
+    }
+
+    /**
+     * @param $formData Department
+     * @param $step
+     * @param array $options
+     * @return array
+     */
+    public function getFormOptions($formData, $step, array $options = array())
+    {
+        $options = parent::getFormOptions($formData, $step, $options);
+
+        $options['cascade_validation'] = true;
+
+        return $options;
+    }
+}
