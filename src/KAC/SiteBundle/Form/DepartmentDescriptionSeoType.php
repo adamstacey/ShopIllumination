73de9968ae8504949d4d9ccb2cf9bf67dc739a14
Adam Stacey <me@adamstacey.co.uk>
2013-03-13 21:59:59 +0000

    Added new department description seo type to update the SEO of a department.

---
+++ src/KAC/SiteBundle/Form/DepartmentDescriptionSeoType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace KAC\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DepartmentDescriptionSeoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text');
+        $builder->add('description', 'textarea');
+        $builder->add('locale', 'locale');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KAC\SiteBundle\Entity\Department\Description'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'site_department_description_seo';
+    }
+}
