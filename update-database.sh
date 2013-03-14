fe3428a058636b4ebfcda4bf8cc8a1d729960e6f
Adam Stacey <me@adamstacey.co.uk>
2013-03-14 22:01:05 +0000

    Added SQL dump to update database script, so any database updates can be added to the database migration update.

--- update-database.sh
+++ update-database.sh
@@ -2,5 +2,6 @@ clear
 
 echo "Synchronising the database:"
 php app/console doctrine:generate:entities KAC
+php app/console doctrine:schema:update --dump-sql
 php app/console doctrine:schema:update --force
 echo "OK!"
