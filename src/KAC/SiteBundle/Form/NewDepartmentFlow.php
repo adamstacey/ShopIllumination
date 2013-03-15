3722b447ce51b4c179db9334c9cf2821318ff331
Adam Stacey <me@adamstacey.co.uk>
2013-03-15 00:09:24 +0000

    Added step 2 to the new department flow.

--- src/KAC/SiteBundle/Form/NewDepartmentFlow.php
+++ src/KAC/SiteBundle/Form/NewDepartmentFlow.php
@@ -15,20 +15,24 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Doctrine\ORM\EntityRepository;
 use Craue\FormFlowBundle\Form\FormFlow;
 use KAC\SiteBundle\Entity\Department;
+use KAC\SiteBundle\Manager\SeoManager;
 
 class NewDepartmentFlow extends FormFlow
 {
-    protected $maxSteps = 1;
+    protected $maxSteps = 2;
     protected $allowDynamicStepNavigation = true;
+
+    private $seoManager;
+
+    public function __construct($doctrine, SeoManager $seoManager)
+    {
+        $this->seoManager = $seoManager;
+    }
 
     protected function loadStepDescriptions() {
         return array(
             '1. Overview',
-            '2. Build Combinations',
-            '3. Prices',
-            '4. Features',
-            '5. Images',
-            '6. Links'
+            '2. SEO'
         );
     }
 
@@ -43,6 +47,30 @@ class NewDepartmentFlow extends FormFlow
         $options = parent::getFormOptions($formData, $step, $options);
 
         $options['cascade_validation'] = true;
+
+        if ($step == 2)
+        {
+            if (!$formData->getDescription()->getHeader())
+            {
+                $formData->getDescription()->setHeader($formData->getDescription()->getName());
+            }
+            if (!$formData->getDescription()->getMenuTitle())
+            {
+                $formData->getDescription()->setMenuTitle($formData->getDescription()->getName());
+            }
+            if (!$formData->getDescription()->getPageTitle())
+            {
+                $formData->getDescription()->setPageTitle($formData->getDescription()->getName());
+            }
+            if (!$formData->getDescription()->getMetaDescription())
+            {
+                $formData->getDescription()->setMetaDescription($this->seoManager->removeHtml($formData->getDescription()->getDescription()));
+            }
+            if (!$formData->getDescription()->getMetaKeywords())
+            {
+                $formData->getDescription()->setMetaKeywords($this->seoManager->generateKeywords($formData->getDescription()->getName().' '.$formData->getDescription()->getDescription()));
+            }
+        }
 
         return $options;
     }
