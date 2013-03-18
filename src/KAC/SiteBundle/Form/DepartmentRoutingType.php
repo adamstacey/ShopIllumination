9bbad5a6e0ef10f90651e06c3c56df46775e7cd3
Adam Stacey <me@adamstacey.co.uk>
2013-03-18 15:30:07 +0000

    Added new department routing type for step 2 of the new department flow.

---
+++ src/KAC/SiteBundle/Form/DepartmentRoutingType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace KAC\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DepartmentRoutingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('url', 'text', array(
+            'required'  => true,
+            'label' => 'Internal Web Address',
+            'attr' => array(
+                'class' => 'fill',
+                'help' => 'The internal web address is automatically generated and is kept up-to-date by the rules setup in the system. However, you can make any changes to enhance the internal web address here.',
+            ),
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KAC\SiteBundle\Entity\Department\Routing'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'site_department_routing';
+    }
+}
