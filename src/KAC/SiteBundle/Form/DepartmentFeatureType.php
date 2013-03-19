22d2da6ea60ad17ff19ee6da48c36381b8f0d1b0
Adam Stacey <me@adamstacey.co.uk>
2013-03-19 17:13:24 +0000

    Fixed issue on department feature type where the features were being removed and added. They are now just updated when the feature group is updated.

--- src/KAC/SiteBundle/Form/DepartmentFeatureType.php
+++ src/KAC/SiteBundle/Form/DepartmentFeatureType.php
@@ -25,27 +25,42 @@ class DepartmentFeatureType extends AbstractType
                 return $er->createQueryBuilder('fg')
                     ->orderBy('fg.name', 'ASC');
             },
+            'attr' => array(
+                'class' => 'feature-group fill',
+            ),
         ));
 
         $builder->add('feature', 'choice', array(
             'required' => false,
             'empty_value' => '- Select a Group First -',
             'choices' => array(),
+            'attr' => array(
+                'class' => 'feature fill',
+            ),
         ));
 
         $builder->add('displayOnFilter', 'checkbox', array(
             'required'  => false,
+            'attr' => array(
+                'containerClass' => 'tac',
+            ),
         ));
 
         $builder->add('displayOnListing', 'checkbox', array(
             'required'  => false,
+            'attr' => array(
+                'containerClass' => 'tac',
+            ),
         ));
 
         $builder->add('displayOnProduct', 'checkbox', array(
             'required'  => false,
+            'attr' => array(
+                'containerClass' => 'tac',
+            ),
         ));
 
-        $builder->add('displayOrder', 'checkbox');
+        $builder->add('displayOrder', 'hidden');
 
         $builder->addEventListener(FormEvents::SET_DATA, function($event) use ($factory) {
             /**
@@ -55,10 +70,9 @@ class DepartmentFeatureType extends AbstractType
             $data = $event->getData();
             $form = $event->getForm();
             if ($data && $data->getFeatureGroup() !== null) {
-                $form->remove('feature');
-                $form->add($factory->createNamed('feature', 'entity', null, array(
+                $form->offsetSet('feature', $factory->createNamed('feature', 'entity', null, array(
                     'required'  => false,
-                    'empty_value' => '- Select a Feature -',
+                    'empty_value' => 'No Default Feature',
                     'class' => 'KAC\SiteBundle\Entity\Product\Feature',
                     'query_builder' => function(EntityRepository $er) use ($data) {
                         $qb = $er->createQueryBuilder('f');
@@ -66,6 +80,9 @@ class DepartmentFeatureType extends AbstractType
                         $qb->orderBy('f.name');
                         return $qb;
                     },
+                    'attr' => array(
+                        'class' => 'feature fill',
+                    ),
                 )));
             }
         });
@@ -78,10 +95,9 @@ class DepartmentFeatureType extends AbstractType
             $form = $event->getForm();
 
             if ($data && isset($data['featureGroup'])) {
-                $form->remove('feature');
-                $form->add($factory->createNamed('feature', 'entity', null, array(
+                $form->offsetSet('feature', $factory->createNamed('feature', 'entity', null, array(
                     'required'  => false,
-                    'empty_value' => '- Select a Feature -',
+                    'empty_value' => 'No Default Feature',
                     'class' => 'KAC\SiteBundle\Entity\Product\Feature',
                     'query_builder' => function(EntityRepository $er) use ($data) {
                         $qb = $er->createQueryBuilder('f');
@@ -89,10 +105,12 @@ class DepartmentFeatureType extends AbstractType
                         $qb->orderBy('f.name');
                         return $qb;
                     },
+                    'attr' => array(
+                        'class' => 'feature fill',
+                    ),
                 )));
             }
         });
-        $builder->add('displayOrder', 'hidden');
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
