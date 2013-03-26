656c39acb37f2337998d2c612bec16c476e5fe77
Adam Stacey <me@adamstacey.co.uk>
2013-03-26 10:45:39 +0000

    Added new form type for handling product feature groups.

---
+++ src/KAC/SiteBundle/Form/ProductFeatureGroupType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace KAC\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+use KAC\SiteBundle\Entity\Product\Feature;
+
+class ProductFeatureGroupType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array(
+            'required'  => true,
+            'label' => 'Name',
+            'attr' => array(
+                'class' => 'fill',
+                'help' => 'Enter the name of the product feature group.',
+            ),
+        ));
+
+        $builder->add('active', 'checkbox', array(
+            'required' => false,
+            'label' => 'Active',
+            'attr' => array(
+                'help' => 'Is the feature group active?',
+            ),
+        ));
+
+        $builder->add('filter', 'checkbox', array(
+            'required' => false,
+            'label' => 'Filter',
+            'attr' => array(
+                'help' => 'Can you filter by the feature group?',
+            ),
+        ));
+
+        $builder->add('departments', 'collection', array(
+            'type' => new ProductDepartmentType(),
+            'mapped' => false,
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KAC\SiteBundle\Entity\Product\FeatureGroup'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'site_product_feature_group';
+    }
+}
