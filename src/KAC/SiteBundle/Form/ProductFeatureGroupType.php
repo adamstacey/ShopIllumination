a119da34da7e4e9cbd2e80a81e4e1f8c21356173
Adam Stacey <me@adamstacey.co.uk>
2013-03-27 16:34:42 +0000

    Manually added a constraint to the product feature type, so it is validated when in a collection.

--- src/KAC/SiteBundle/Form/ProductFeatureGroupType.php
+++ src/KAC/SiteBundle/Form/ProductFeatureGroupType.php
@@ -4,6 +4,7 @@ namespace KAC\SiteBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -23,6 +24,9 @@ class ProductFeatureGroupType extends AbstractType
                 'class' => 'fill',
                 'help' => 'Enter the name of the product feature group.',
                 'placeholder' => 'Feature Group',
+            ),
+            'constraints' => array(
+                new NotBlank(array('message' => 'Enter a name for the feature group.')),
             ),
         ));
 
@@ -50,7 +54,7 @@ class ProductFeatureGroupType extends AbstractType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'KAC\SiteBundle\Entity\Product\FeatureGroup'
+            'data_class' => 'KAC\SiteBundle\Entity\Product\FeatureGroup',
         ));
     }
 
