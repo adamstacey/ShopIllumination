0b30ae732eb9619373137725020a0f0c5574dddf
Adam Stacey <me@adamstacey.co.uk>
2013-04-08 12:22:34 +0100

    Updated the classes and helpers on the product price type.

--- src/KAC/SiteBundle/Form/ProductPriceType.php
+++ src/KAC/SiteBundle/Form/ProductPriceType.php
@@ -15,20 +15,29 @@ class ProductPriceType extends AbstractType
             'currency' => false,
             'attr' => array(
                 'size' => 6,
+                'class' => 'fill float tac',
+                'data-help' => 'Enter the cost price of the product.',
             ),
         ));
+
         $builder->add('recommendedRetailPrice', 'money', array(
             'currency' => false,
             'attr' => array(
                 'size' => 6,
+                'class' => 'fill float tac',
+                'data-help' => 'Enter the recommended retail price (RRP) of the product. Leave it blank if you want the RRP to be the list price.',
             ),
         ));
+
         $builder->add('listPrice', 'money', array(
             'currency' => false,
             'attr' => array(
                 'size' => 6,
+                'class' => 'fill float tac',
+                'data-help' => 'Enter the list price. This is the price the product will sell for.',
             ),
         ));
+
         $builder->add('displayOrder', 'hidden');
     }
 
