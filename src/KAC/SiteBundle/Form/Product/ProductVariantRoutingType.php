dc175109de3df00e26ddd3ed2ddd560e89099070
Adam Stacey <me@adamstacey.co.uk>
2013-04-19 15:50:39 +0100

    Added new form type to handle the routing for a product variant.

---
+++ src/KAC/SiteBundle/Form/Product/ProductVariantRoutingType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace KAC\SiteBundle\Form\Product;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProductVariantRoutingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('url', 'text', array(
+            'required'  => true,
+            'label' => 'Internal Web Address',
+            'attr' => array(
+                'class' => 'fill routing',
+                'data-help' => 'The internal web address is automatically generated and is kept up-to-date by the rules setup in the system. However, you can make any changes to enhance the internal web address here.',
+            ),
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KAC\SiteBundle\Entity\Product\Variant\Routing'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'site_product_variant_routing';
+    }
+}
