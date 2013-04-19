4e7b19982fea7b334ae37bc768a919f5fbece51f
Adam Stacey <me@adamstacey.co.uk>
2013-04-19 15:50:26 +0100

    Added new form type to handle the description SEO for a product variant.

---
+++ src/KAC/SiteBundle/Form/Product/ProductVariantDescriptionSeoType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace KAC\SiteBundle\Form\Product;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
+
+class ProductVariantDescriptionSeoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('pageTitle', 'text', array(
+            'required'  => true,
+            'label' => '<span class="important">Page Title</span>',
+            'attr' => array(
+                'class' => 'fill recommended-characters',
+                'data-help' => 'VERY IMPORTANT: The page title is the title used in the Browser window and more importantly the title in search engine results pages (SERPs). This should be more specific than the header.',
+                'data-character-help' => 70,
+            ),
+        ));
+
+        $builder->add('header', 'text', array(
+            'required'  => true,
+            'label' => '<span class="important">Header</span>',
+            'attr' => array(
+                'class' => 'fill',
+                'data-help' => 'VERY IMPORTANT: The header is the main header used in listings and on the pages in the site. This should exclude specifics and be more general than the page title.',
+            ),
+        ));
+
+        $builder->add('metaDescription', 'textarea', array(
+            'required'  => true,
+            'label' => '<span class="important">Meta Description</span>',
+            'attr' => array(
+                'class' => 'fill recommended-characters',
+                'data-help' => 'VERY IMPORTANT: Your Advert in organic search results. You need to include keywords, but primarily the meta description needs to entice people to click through by using a call to action with benefits (e.g. lowest price, in stock, free delivery, etc).',
+                'data-character-help' => 160,
+            ),
+        ));
+
+        $builder->add('metaKeywords', 'textarea', array(
+            'label' => 'Search Words',
+            'attr' => array(
+                'class' => 'fill',
+                'data-help' => 'Enter any alternative words to be found by (separated by a comma). This could be slight differences in wording or phrasing and should include all keywords used in the page title.',
+            ),
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KAC\SiteBundle\Entity\Product\Variant\Description'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'site_product_variant_description_seo';
+    }
+}
