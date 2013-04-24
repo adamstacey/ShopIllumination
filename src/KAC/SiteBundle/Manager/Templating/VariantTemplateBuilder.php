6b1ac7b96e9feff597dc5a69465ad47ba676d656
Adam Stacey <me@adamstacey.co.uk>
2013-04-24 12:43:44 +0100

    Fixed an issue with the abstract get entity function in the variant template builder where the entity could not be found for a variant that had not yet been saved, i.e. in a new form flow.

--- src/KAC/SiteBundle/Manager/Templating/VariantTemplateBuilder.php
+++ src/KAC/SiteBundle/Manager/Templating/VariantTemplateBuilder.php
@@ -13,15 +13,24 @@ class VariantTemplateBuilder extends TemplateBuilder
 
     function getEntity($object, $alias, $id)
     {
-        if($alias === 'VariantToFeature')
+        if ($alias == 'VariantToFeature')
         {
-            $query = $this->em->createQuery("SELECT f FROM KAC\SiteBundle\Entity\Product\Feature f WHERE f = (SELECT IDENTITY(vtf.feature) FROM KAC\SiteBundle\Entity\Product\VariantToFeature vtf WHERE vtf.featureGroup = ?1 AND vtf.variant = ?2)");
-            $query->setParameter(1, $id);
-            $query->setParameter(2, $object->getVariant()->getId());
-            $objects = $query->execute();
+            if ($object->getVariant())
+            {
+                $featureId = 0;
+                foreach ($object->getVariant()->getFeatures() as $feature)
+                {
+                    if ($feature->getFeatureGroup()->getId() == $id)
+                    {
+                        $featureId = $feature->getFeature()->getId();
+                        break;
+                    }
+                }
+            }
+            $feature = $this->em->getRepository('KAC\SiteBundle\Entity\Product\Feature')->find($featureId);
 
-            if($objects) {
-                return $objects[0];
+            if ($feature) {
+                return $feature;
             }
         }
 
