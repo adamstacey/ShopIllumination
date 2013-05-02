5e3e5f28de21281cbfed85ff1b0e4e3f4ee0ef0a
Adam Stacey <me@adamstacey.co.uk>
2013-05-02 17:23:36 +0100

    Updated the update product description to take the common items from the variants and build the routing.

--- src/KAC/SiteBundle/Manager/ProductManager.php
+++ src/KAC/SiteBundle/Manager/ProductManager.php
@@ -33,10 +33,7 @@ class ProductManager extends Manager
 
         $product->addDescription(new ProductDescription());
         $product->addDepartment(new ProductToDepartment());
-        $product->addPrice(new Price());
         $product->addRouting(new ProductRouting());
-
-        $product->addFeatureGroup(new VariantToFeature());
 
         return $product;
     }
@@ -45,48 +42,95 @@ class ProductManager extends Manager
     {
         $variant = new Variant();
 
-        $variant->setProductCode($product->getProductCode());
         $variant->setProduct($product);
         $variant->addDescription(new VariantDescription());
         $variant->addPrice(new Product\Price());
+        $variant->addRouting(new ProductVariantRouting());
 
         return $variant;
     }
 
     public function updateProductDescription(ProductDescription $description)
     {
-        if (!$description->getProduct()) return;
+        $product = $description->getProduct();
+        if (!$product) return;
 
-        // Get objects
-        $brand = $description->getProduct()->getBrand();
-        $department = $description->getProduct()->getDepartment()->getDepartment();
+        // Check if we should automatically update the description
+        if ($description->getOverride()) return;
 
-        $builder = new ProductTemplateBuilder($this->doctrine->getManager());
-        $pageTitle = $builder->buildString($description, $department->getDescription()->getPageTitleTemplate());
-        $header = $builder->buildString($description, $department->getDescription()->getHeaderTemplate());
-        $metaDescription = $builder->buildString($description, $department->getDescription()->getMetaDescriptionTemplate());
+        // Get the product variants
+        $variants = $product->getVariants();
+        if (sizeof($variants) < 1) return;
 
-        /* @var \KAC\SiteBundle\Entity\Department $department*/
-
-        if($brand && $department)
+        // Go through variants and put together the description data
+        if (sizeof($variants) == 1)
         {
-            $metaKeywords = $description->getProduct()->getProductCode();
-
-            if ($brand->getDescription())
+            $variant = $product->getVariant();
+            foreach ($variant->getDescriptions() as $variantDescription)
             {
-                $metaKeywords .= ' '.$brand->getDescription()->getName();
+                if ($variantDescription->getLocale() == $description->getlocale())
+                {
+                    $searches = array($variant->getProductCode(), "  ");
+                    $replacements = array("", " ");
+                    $commonPageTitle = str_replace($searches, $replacements, $variantDescription->getPageTitle());
+                    $commonHeader = str_replace($searches, $replacements, $variantDescription->getHeader());
+                    $commonMetaDescription = str_replace($searches, $replacements, $variantDescription->getMetaDescription());
+                    $commonMetaKeywords = $variantDescription->getMetaKeywords();
+                }
             }
-            if ($department->getDescription())
+        } else {
+            $pageTitles = array();
+            $headers = array();
+            $metaDescriptions = array();
+            $metaKeywords = array();
+            foreach ($variants as $variant)
             {
-                $metaKeywords .= ' '.$department->getDescription()->getName();
+                foreach ($variant->getDescriptions() as $variantDescription)
+                {
+                    if ($variantDescription->getLocale() == $description->getlocale())
+                    {
+                        $pageTitles[] = explode(' ', $variantDescription->getPageTitle());
+                        $headers[] = explode(' ', $variantDescription->getHeader());
+                        $metaDescriptions[] = explode(' ', $variantDescription->getMetaDescription());
+                        foreach (explode(',', $variantDescription->getMetaKeywords()) as $metaKeyword)
+                        {
+                            $metaKeywords[] = trim($metaKeyword);
+                        }
+                    }
+                }
             }
 
-            $description->setName($pageTitle);
-            $description->setPageTitle($pageTitle);
-            $description->setHeader($header);
-            $description->setMetaDescription($metaDescription);
-            $description->setMetaKeywords($this->seoManager->generateKeywords($metaKeywords));
-            $description->setShortDescription($this->seoManager->shortenContent($description->getDescription(), 160));
+            // Get the common data
+            $commonPageTitle = implode(' ', call_user_func_array('array_intersect', $pageTitles));
+            $commonHeader = implode(' ', call_user_func_array('array_intersect', $headers));
+            $commonMetaDescription = implode(' ', call_user_func_array('array_intersect', $metaDescriptions));
+            $commonMetaKeywords = implode(', ',array_unique($metaKeywords));
+        }
+
+        // Update the description
+        $description->setPageTitle($commonPageTitle);
+        $description->setHeader($commonHeader);
+        $description->setMetaDescription($commonMetaDescription);
+        $description->setMetaKeywords($commonMetaKeywords);
+
+        // Update the URL
+        if (sizeof($product->getRoutings()) > 0)
+        {
+            foreach ($product->getRoutings() as $routing)
+            {
+                if ($routing->getLocale() == $description->getLocale())
+                {
+                    $url = $this->seoManager->createUrl($commonPageTitle, $routing->getUrl());
+                    $routing->setUrl($url);
+                }
+            }
+        } else {
+            $url = $this->seoManager->createUrl($commonPageTitle);
+            $routing = new ProductRouting();
+            $routing->setProduct($product);
+            $routing->setLocale($description->getLocale());
+            $routing->setUrl($url);
+            $product->addRouting($routing);
         }
     }
 
