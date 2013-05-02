1bd9ad9b4f57c572a3881711a4dbab80fc56b6dd
Adam Stacey <me@adamstacey.co.uk>
2013-05-02 17:16:39 +0100

    Major updates to the product entity, removing redundant fields and tidying up getters and setters.

--- src/KAC/SiteBundle/Entity/Product.php
+++ src/KAC/SiteBundle/Entity/Product.php
@@ -66,21 +66,6 @@ class Product implements DescribableInterface
     private $status = 'a';
 
     /**
-     * @ORM\Column(name="product_code", type="string", length=100, nullable=true)
-     */
-    private $productCode = '';
-
-    /**
-     * @ORM\Column(name="alternative_product_codes", type="text", nullable=true)
-     */
-    private $alternativeProductCodes = '';
-
-    /**
-     * @ORM\Column(name="checked", type="boolean")
-     */
-    private $checked = false;
-
-    /**
      * @ORM\Column(name="available_for_purchase", type="boolean")
      *
      */
@@ -128,48 +113,7 @@ class Product implements DescribableInterface
      */
     private $sampleRequest = false;
 
-    /**
-     * @ORM\Column(name="hide_price", type="boolean")
-     *
-     */
-    private $hidePrice = false;
 
-    /**
-     * @ORM\Column(name="show_price_out_of_hours", type="boolean")
-     *
-     */
-    private $showPriceOutOfHours = false;
-
-    /**
-     * @ORM\Column(name="membership_card_discount_available", type="boolean")
-     *
-     */
-    private $membershipCardDiscountAvailable = false;
-
-    /**
-     * @ORM\Column(name="maximum_membership_card_discount", type="decimal", precision=12, scale=4, nullable=true)
-     */
-    private $maximumMembershipCardDiscount = 0;
-
-    /**
-     * @ORM\Column(name="delivery_band", type="decimal", precision=12, scale=4)
-     */
-    private $deliveryBand = 1;
-
-    /**
-     * @ORM\Column(name="inherited_delivery_band", type="decimal", precision=12, scale=4, nullable=true)
-     */
-    private $inheritedDeliveryBand = null;
-
-    /**
-     * @ORM\Column(name="delivery_cost", type="decimal", precision=12, scale=4)
-     */
-    private $deliveryCost = 0;
-
-    /**
-     * @ORM\Column(name="last_checked", type="datetime", nullable=true)
-     */
-    private $lastChecked;
 
     /**
      * @ORM\Column(name="template", type="string", length=255)
@@ -193,9 +137,8 @@ class Product implements DescribableInterface
      */
     private $deletedAt;
 
-    private $features = array();
-    private $prices = array();
     private $images = "";
+    private $features = array();
     private $productImages = "";
 
     /**
@@ -226,7 +169,6 @@ class Product implements DescribableInterface
         $this->departments = new \Doctrine\Common\Collections\ArrayCollection();
         $this->links = new \Doctrine\Common\Collections\ArrayCollection();
         $this->features = new \Doctrine\Common\Collections\ArrayCollection();
-        $this->prices = new \Doctrine\Common\Collections\ArrayCollection();
         $this->routings = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
@@ -276,29 +218,6 @@ class Product implements DescribableInterface
     public function getStatus()
     {
         return $this->status;
-    }
-
-    /**
-     * Set checked
-     *
-     * @param boolean $checked
-     * @return Product
-     */
-    public function setChecked($checked)
-    {
-        $this->checked = $checked;
-
-        return $this;
-    }
-
-    /**
-     * Get checked
-     *
-     * @return boolean
-     */
-    public function getChecked()
-    {
-        return $this->checked;
     }
 
     /**
@@ -483,190 +402,6 @@ class Product implements DescribableInterface
     public function getSampleRequest()
     {
         return $this->sampleRequest;
-    }
-
-    /**
-     * Set hidePrice
-     *
-     * @param boolean $hidePrice
-     * @return Product
-     */
-    public function setHidePrice($hidePrice)
-    {
-        $this->hidePrice = $hidePrice;
-
-        return $this;
-    }
-
-    /**
-     * Get hidePrice
-     *
-     * @return boolean
-     */
-    public function getHidePrice()
-    {
-        return $this->hidePrice;
-    }
-
-    /**
-     * Set showPriceOutOfHours
-     *
-     * @param boolean $showPriceOutOfHours
-     * @return Product
-     */
-    public function setShowPriceOutOfHours($showPriceOutOfHours)
-    {
-        $this->showPriceOutOfHours = $showPriceOutOfHours;
-
-        return $this;
-    }
-
-    /**
-     * Get showPriceOutOfHours
-     *
-     * @return boolean
-     */
-    public function getShowPriceOutOfHours()
-    {
-        return $this->showPriceOutOfHours;
-    }
-
-    /**
-     * Set membershipCardDiscountAvailable
-     *
-     * @param boolean $membershipCardDiscountAvailable
-     * @return Product
-     */
-    public function setMembershipCardDiscountAvailable($membershipCardDiscountAvailable)
-    {
-        $this->membershipCardDiscountAvailable = $membershipCardDiscountAvailable;
-
-        return $this;
-    }
-
-    /**
-     * Get membershipCardDiscountAvailable
-     *
-     * @return boolean
-     */
-    public function getMembershipCardDiscountAvailable()
-    {
-        return $this->membershipCardDiscountAvailable;
-    }
-
-    /**
-     * Set maximumMembershipCardDiscount
-     *
-     * @param float $maximumMembershipCardDiscount
-     * @return Product
-     */
-    public function setMaximumMembershipCardDiscount($maximumMembershipCardDiscount)
-    {
-        $this->maximumMembershipCardDiscount = $maximumMembershipCardDiscount;
-
-        return $this;
-    }
-
-    /**
-     * Get maximumMembershipCardDiscount
-     *
-     * @return float
-     */
-    public function getMaximumMembershipCardDiscount()
-    {
-        return $this->maximumMembershipCardDiscount;
-    }
-
-    /**
-     * Set deliveryBand
-     *
-     * @param float $deliveryBand
-     * @return Product
-     */
-    public function setDeliveryBand($deliveryBand)
-    {
-        $this->deliveryBand = $deliveryBand;
-
-        return $this;
-    }
-
-    /**
-     * Get deliveryBand
-     *
-     * @return float
-     */
-    public function getDeliveryBand()
-    {
-        return $this->deliveryBand;
-    }
-
-    /**
-     * Set inheritedDeliveryBand
-     *
-     * @param float $inheritedDeliveryBand
-     * @return Product
-     */
-    public function setInheritedDeliveryBand($inheritedDeliveryBand)
-    {
-        $this->inheritedDeliveryBand = $inheritedDeliveryBand;
-
-        return $this;
-    }
-
-    /**
-     * Get inheritedDeliveryBand
-     *
-     * @return float
-     */
-    public function getInheritedDeliveryBand()
-    {
-        return $this->inheritedDeliveryBand;
-    }
-
-    /**
-     * Set deliveryCost
-     *
-     * @param float $deliveryCost
-     * @return Product
-     */
-    public function setDeliveryCost($deliveryCost)
-    {
-        $this->deliveryCost = $deliveryCost;
-
-        return $this;
-    }
-
-    /**
-     * Get deliveryCost
-     *
-     * @return float
-     */
-    public function getDeliveryCost()
-    {
-        return $this->deliveryCost;
-    }
-
-    /**
-     * Set lastChecked
-     *
-     * @param \DateTime $lastChecked
-     * @return Product
-     */
-    public function setLastChecked($lastChecked)
-    {
-        $this->lastChecked = $lastChecked;
-
-        return $this;
-    }
-
-    /**
-     * Get lastChecked
-     *
-     * @return \DateTime
-     */
-    public function getLastChecked()
-    {
-        return $this->lastChecked;
     }
 
     /**
@@ -869,6 +604,21 @@ class Product implements DescribableInterface
     }
 
     /**
+     * Get variant
+     *
+     * @return Product\Variant
+     */
+    public function getVariant()
+    {
+        if(count($this->variants) > 0)
+        {
+            return $this->variants[0];
+        }
+
+        return null;
+    }
+
+    /**
      * Add descriptions
      *
      * @param \KAC\SiteBundle\Entity\Product\Description $descriptions
@@ -915,74 +665,6 @@ class Product implements DescribableInterface
         }
 
         return null;
-    }
-
-    public function getProductCode()
-    {
-        return $this->productCode;
-    }
-
-    public function setProductCode($productCode)
-    {
-        $this->productCode = $productCode;
-    }
-
-    public function getFeatures()
-    {
-        return $this->features;
-    }
-
-    public function setFeatures($featureGroups)
-    {
-        $this->features = $featureGroups;
-    }
-
-    public function addFeatureGroup(Product\VariantToFeature $featureGroup)
-    {
-        $this->features[] = $featureGroup;
-    }
-
-    public function removeFeatureGroup($featureGroup)
-    {
-        $key = array_search($featureGroup, $this->features);
-        if($key)
-        {
-            unset($this->features[$key]);
-        }
-    }
-
-    public function getPrices()
-    {
-        return $this->prices;
-    }
-
-    public function setPrices($prices)
-    {
-        $this->prices = $prices;
-    }
-
-    public function addPrice($price)
-    {
-        $this->prices[] = $price;
-    }
-
-    public function removePrice($price)
-    {
-        $key = array_search($price, $this->prices);
-        if($key)
-        {
-            unset($this->prices[$key]);
-        }
-    }
-
-    public function getAlternativeProductCodes()
-    {
-        return $this->alternativeProductCodes;
-    }
-
-    public function setAlternativeProductCodes($alternativeProductCodes)
-    {
-        $this->alternativeProductCodes = $alternativeProductCodes;
     }
 
     public function getDeletedAt()
@@ -1090,4 +772,29 @@ class Product implements DescribableInterface
     {
         return $this->routings;
     }
+
+    public function getFeatures()
+    {
+        return $this->features;
+    }
+
+    public function setFeatures($featureGroups)
+    {
+        $this->features = $featureGroups;
+    }
+
+    public function addFeatureGroup(Product\VariantToFeature $featureGroup)
+    {
+        $this->features[] = $featureGroup;
+    }
+
+    public function removeFeatureGroup($featureGroup)
+    {
+        $key = array_search($featureGroup, $this->features);
+        if($key)
+        {
+            unset($this->features[$key]);
+        }
+    }
+
 }
