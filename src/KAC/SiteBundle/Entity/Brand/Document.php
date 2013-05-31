bbda799266910a135624259c030ce4bb9dae6b3e
Adam Stacey <me@adamstacey.co.uk>
2013-05-31 17:22:10 +0100

    Added the inherited document entity for the brand entity.

---
+++ src/KAC/SiteBundle/Entity/Brand/Document.php
@@ -0,0 +1,63 @@
+<?php
+namespace KAC\SiteBundle\Entity\Brand;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use \KAC\SiteBundle\Entity\Document as SiteDocument;
+
+/**
+ * @ORM\Entity
+ */
+class Document extends SiteDocument
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer", length=11)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="KAC\SiteBundle\Entity\Brand", inversedBy="images")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id")
+     */
+    protected $brand;
+
+    /**
+     * Set brand
+     *
+     * @param \KAC\SiteBundle\Entity\Brand $brand
+     * @return Routing
+     */
+    public function setBrand(\KAC\SiteBundle\Entity\Brand $brand = null)
+    {
+        $this->brand = $brand;
+    
+        return $this;
+    }
+
+    /**
+     * Get brand
+     *
+     * @return \KAC\SiteBundle\Entity\Brand
+     */
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    public function getObjectType()
+    {
+        return 'brand';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
