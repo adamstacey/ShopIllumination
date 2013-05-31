4925fa16f418638e2f9f672ef7df9d1f947e6461
Adam Stacey <me@adamstacey.co.uk>
2013-05-31 17:22:36 +0100

    Added the inherited document entity for the product entity.

---
+++ src/KAC/SiteBundle/Entity/Product/Document.php
@@ -0,0 +1,63 @@
+<?php
+namespace KAC\SiteBundle\Entity\Product;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use \KAC\SiteBundle\Entity\Document as SiteDocument;
+
+/**
+ * @ORM\Entity
+ */
+class Document extends SiteDocument
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer", length=11)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="KAC\SiteBundle\Entity\Product", inversedBy="documents")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id")
+     */
+    protected $product;
+
+    /**
+     * Set product
+     *
+     * @param \KAC\SiteBundle\Entity\Product $product
+     * @return Routing
+     */
+    public function setProduct(\KAC\SiteBundle\Entity\Product $product = null)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return \KAC\SiteBundle\Entity\Product
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    public function getObjectType()
+    {
+        return 'product';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
