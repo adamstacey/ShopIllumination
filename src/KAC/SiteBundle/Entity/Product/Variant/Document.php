494d8f3ccf2e8c83866a07b142dd4f941d316f21
Adam Stacey <me@adamstacey.co.uk>
2013-05-31 17:21:57 +0100

    Added the inherited document entity for the product variant entity.

---
+++ src/KAC/SiteBundle/Entity/Product/Variant/Document.php
@@ -0,0 +1,63 @@
+<?php
+namespace KAC\SiteBundle\Entity\Product\Variant;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use \KAC\SiteBundle\Entity\Document as SiteDocument;
+
+/**
+ * @ORM\Entity
+ */
+class Document extends SiteDocument
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer", length=11)
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="KAC\SiteBundle\Entity\Product\Variant", inversedBy="documents")
+     * @ORM\JoinColumn(name="object_id", referencedColumnName="id")
+     */
+    protected $variant;
+
+    /**
+     * Set product
+     *
+     * @param \KAC\SiteBundle\Entity\Product\Variant $variant
+     * @return Routing
+     */
+    public function setVariant(\KAC\SiteBundle\Entity\Product\Variant $variant = null)
+    {
+        $this->variant = $variant;
+
+        return $this;
+    }
+
+    /**
+     * Get variant
+     *
+     * @return \KAC\SiteBundle\Entity\Product\Variant
+     */
+    public function getVariant()
+    {
+        return $this->variant;
+    }
+
+    public function getObjectType()
+    {
+        return 'product_variant';
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
