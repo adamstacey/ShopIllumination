763f6de769f59730f06ad4d0c5afb3d23f644370
Adam Stacey <me@adamstacey.co.uk>
2013-06-03 01:19:44 +0100

    Renamed the temporary documents field in the product variant document form type.

--- src/KAC/SiteBundle/Form/Product/ProductVariantDocumentsType.php
+++ src/KAC/SiteBundle/Form/Product/ProductVariantDocumentsType.php
@@ -11,7 +11,7 @@ class ProductVariantDocumentsType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('documents', 'hidden');
+        $builder->add('temporaryDocuments', 'hidden');
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
