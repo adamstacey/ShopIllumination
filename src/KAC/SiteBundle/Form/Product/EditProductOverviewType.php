f0dc7ee0310ed6ec2f820868dd5b5531dc200c28
Daniel Cannon <daniel@danielcannon.co.uk>
2013-06-04 11:36:09 +0100

    Updated edit product overview page

--- src/KAC/SiteBundle/Form/Product/EditProductOverviewType.php
+++ src/KAC/SiteBundle/Form/Product/EditProductOverviewType.php
@@ -16,27 +16,88 @@ class EditProductOverviewType extends AbstractType
             'query_builder' => function(EntityRepository $er) {
                 return $er->createQueryBuilder('b')
                     ->addSelect('bd')
-                    ->leftJoin('b.descriptions', 'bd');
+                    ->leftJoin('b.descriptions', 'bd')
+                    ->orderBy('bd.name');
             },
+            'label' => 'Brand',
+            'attr' => array(
+                'class' => 'fill select-brand',
+                'data-help' => 'Select the department you want this department to fall under.',
+                'data-placeholder' => '- Select a Brand -',
+                'placeholder' => '- Select a Brand -',
+            ),
+            'required' => true,
+            'empty_value' => '- Select a Brand -',
         ), array());
         $builder->add('departments', 'collection', array(
             'type' => new ProductDepartmentType(),
         ));
         $builder->add('status', 'choice', array(
+            'label' => 'Status',
             'choices' => array('a' => 'Available', 'h' => 'Hidden', 'd' => 'Disabled'),
-            'label'  => 'Due Date',
+            'required' => true,
+            'attr' => array(
+                'data-help' => 'Select the status of the department. Any sub-departments will also inherit this status.',
+            ),
         ));
-        $builder->add('availableForPurchase');
-        $builder->add('featureComparison');
-        $builder->add('downloadable');
-        $builder->add('specialOffer');
-        $builder->add('recommended');
-        $builder->add('accessory');
-        $builder->add('new');
-
-        // Prices
-        $builder->add('hidePrice');
-        $builder->add('showPriceOutOfHours');
+        $builder->add('template', 'choice', array(
+            'label' => 'Template',
+            'choices' => array('default' => 'Standard Product Template', 'ducting' => 'Ducting Product Template', 'lighting' => 'Lighting Product Template', 'worktop' => 'Worktop Product Template'),
+            'required' => true,
+            'attr' => array(
+                'class' => 'fill',
+                'data-help' => 'Select the template the product will use when a user visits the product.',
+            ),
+        ));
+        $builder->add('availableForPurchase', 'checkbox', array(
+            'required' => false,
+            'label' => 'Available',
+            'attr' => array(
+                'data-help' => 'Is the product available to purchase?',
+            ),
+        ));
+        $builder->add('featureComparison', 'checkbox', array(
+            'required' => false,
+            'label' => 'Feature Comparison',
+            'attr' => array(
+                'data-help' => 'Include in feature comparison?',
+            ),
+        ));
+        $builder->add('downloadable', 'checkbox', array(
+            'required' => false,
+            'label' => 'Downloadable',
+            'attr' => array(
+                'data-help' => 'Is the product a download?',
+            ),
+        ));
+        $builder->add('specialOffer', 'checkbox', array(
+            'required' => false,
+            'label' => 'Special Offer',
+            'attr' => array(
+                'data-help' => 'Is the product a special offer?',
+            ),
+        ));
+        $builder->add('recommended', 'checkbox', array(
+            'required' => false,
+            'label' => 'Recommended',
+            'attr' => array(
+                'data-help' => 'Do you want to recommend?',
+            ),
+        ));
+        $builder->add('accessory', 'checkbox', array(
+            'required' => false,
+            'label' => 'Accessory',
+            'attr' => array(
+                'data-help' => 'Is the product an accessory?',
+            ),
+        ));
+        $builder->add('new', 'checkbox', array(
+            'required' => false,
+            'label' => 'New',
+            'attr' => array(
+                'data-help' => 'Is the product new?',
+            ),
+        ));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
