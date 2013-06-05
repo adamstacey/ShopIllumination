5907e43625b8fffb487bf850032f9a79f8c51c8a
Adam Stacey <me@adamstacey.co.uk>
2013-06-05 16:44:08 +0100

    Updated a number of the functions in the image manager to re-build the images for an object each update. Added some additional helping functions.

--- src/KAC/SiteBundle/Manager/ImageManager.php
+++ src/KAC/SiteBundle/Manager/ImageManager.php
@@ -10,8 +10,8 @@ use KAC\SiteBundle\Entity\Product\Image as ProductImage;
 use KAC\SiteBundle\Entity\Product\Variant\Image as ProductVariantImage;
 use KAC\SiteBundle\Entity\Brand\Image as BrandImage;
 use KAC\SiteBundle\Entity\Department\Image as DepartmentImage;
-use KAC\SiteBundle\Entity\ProductToDepartment;
-use KAC\SiteBundle\Entity\Routing;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 class ImageManager extends Manager
 {
@@ -48,88 +48,180 @@ class ImageManager extends Manager
 
     public function process(Image $image, DescribableInterface $object)
     {
-        $imagine = new Imagine();
+        // Set up defaults where we can
+        if (!$image->getTitle())
+        {
+            $image->setTitle($object->getDescription()->getHeader());
+        }
+        if (!$image->getDescription())
+        {
+            $image->setDescription($object->getDescription()->getMetaDescription());
+        }
+        if (!$image->getImageType())
+        {
+            $image->setImageType($image->getObjectType());
+        }
+
+        // Get the file information
         $filename = $this->fileManager->cleanFilename($object->getDescription()->getHeader());
         $basePath = $image->getUploadDir().'/'.$image->getObjectType().'/'.$image->getImageType();
         $filePath = $basePath.'/'.$filename.'-'.$image->getId().'.jpg';
-        $fileSize = filesize($filePath);
         $image->setPublicPath($filePath);
         $image->setFileExtension('jpg');
-        $image->setFileSize($fileSize);
+
+        // Process the image
+        $imagine = new Imagine();
         $imagine
             ->open($image->getUploadPath().$image->getOriginalPath())
             ->save($image->getUploadPath().$image->getPublicPath());
+
+        // Get the updated file size
+        $fileSize = filesize($filePath);
+        $image->setFileSize($fileSize);
     }
 
-    public function persistImages($entity, $entityType)
+    public function persistImages($object, $objectType)
+    {
+        $fs = new Filesystem();
+        $em = $this->doctrine->getManager();
+
+        // Unlink the existing images
+        foreach ($object->getImages() as $image)
+        {
+            $object->removeImage($image);
+        }
+
+        // Go through the temporary images
+        $imageIds = explode(',', $object->getTemporaryImages());
+        $displayOrder = 0;
+        foreach ($imageIds as $imageId)
+        {
+            $image = $em->getRepository($this->getClassName($objectType))->find($imageId);
+            if ($image)
+            {
+                // Get a new original filename and copy the file
+                $filename = sha1(uniqid(mt_rand(), true));
+                $originalPath = $image->getUploadDir().'/'.$filename.'.'.$this->fileManager->getFileExtension($image->getOriginalPath());
+                try {
+                    $fs->copy($image->getUploadPath().$image->getOriginalPath(), $image->getUploadPath().$originalPath, true);
+                } catch (IOException $e) {
+                    throw new \Exception('An error occurred while copying the image '.$image->getUploadPath().$image->getOriginalPath().' to '.$image->getUploadPath().$originalPath.'.');
+                }
+
+                // Create a new image based on the object type
+                switch ($objectType)
+                {
+                    case 'brand':
+                        $newImage = new BrandImage();
+                        $newImage->setBrand($object);
+                        break;
+                    case 'department':
+                        $newImage = new DepartmentImage();
+                        $newImage->setDepartment($object);
+                        break;
+                    case 'product_variant':
+                        $newImage = new ProductVariantImage();
+                        $newImage->setVariant($object);
+                        break;
+                    case 'product':
+                    default:
+                        $newImage = new ProductImage();
+                        $newImage->setProduct($object);
+                        break;
+                }
+
+                // Copy the data from the temporary image
+                $newImage->setLocale($image->getLocale());
+                $newImage->setTitle($image->getTitle());
+                $newImage->setAlignment($image->getAlignment());
+                $newImage->setDescription($image->getDescription());
+                $newImage->setLink($image->getLink());
+                $newImage->setImageType($image->getImageType());
+                $newImage->setDisplayOrder($displayOrder);
+                $newImage->setOriginalPath($originalPath);
+
+                // Process the new image and add it to the object
+                $this->process($newImage, $object);
+                $object->addImage($newImage);
+                $displayOrder++;
+            }
+        }
+    }
+
+    public function removeTemporaryImages($object)
     {
         /**
          * @var $em EntityManager
          * @var $image Image
          */
         $em = $this->doctrine->getManager();
-        $imageIds = array_diff(explode(',', $entity->getImages()), array(''));
 
-        // Get any images already linked to the entity
-        $existingImages = $em->getRepository("KAC\SiteBundle\Entity\Image")->findBy(array(
-            'objectId' => $entity->getId(),
-            'objectType' => $entityType,
-        ));
-
-        // Delete any old images that no longer exist
-        foreach ($existingImages as $existingImage)
+        // Remove the temporary uploads
+        $uploadIds = explode(',', $object->getImageUploads());
+        foreach ($uploadIds as $uploadId)
         {
-            $found = false;
-            foreach ($imageIds as $imageId)
+            $upload = $em->getRepository("KAC\SiteBundle\Entity\Image")->find($uploadId);
+            if ($upload)
             {
-                if ($existingImage->getId() == $imageId)
-                {
-                    $found = true;
-                    break;
-                }
-            }
-            if (!$found) {
-                $em->remove($existingImage);
-                $em->flush();
+                $em->remove($upload);
             }
         }
 
-        // If no images were added add a blank image
-        if (count($imageIds) == 0)
+        // Remove the temporary images
+        $imageIds = explode(',', $object->getTemporaryImages());
+        foreach ($imageIds as $imageId)
         {
-            $image = new Image();
-            $image->setObjectType($entityType);
-            $image->setObjectId($entity->getId());
-            $image->setImageType($entityType);
-            $image->setLocale('en_GB');
-            $image->setTitle($entity->getDescription()->getHeader());
-            $image->setDescription('');
-            $image->setAlignment('');
-            $image->setLink('');
-            $image->setOriginalPath('/images/no-image.jpg');
-            $image->setPublicPath('/images/no-image.jpg');
-            $image->setDisplayOrder(1);
-            $em->persist($image);
-        } else {
-            // Link each image
-            $displayOrder = 1;
-            foreach ($imageIds as $imageId)
+            $image = $em->getRepository("KAC\SiteBundle\Entity\Image")->find($imageId);
+            if ($image)
             {
-                $image = $em->getRepository("KAC\SiteBundle\Entity\Image")->find($imageId);
-                if ($image)
-                {
-                    $image->setObjectType($entityType);
-                    $image->setObjectId($entity->getId());
-                    if($image->getImageType() === "" || $image->getImageType() === null)
-                    {
-                        $image->setImageType($entityType);
-                    }
-                    $image->setPublicPath("");
-                    $image->setDisplayOrder($displayOrder);
-                    $em->persist($image);
-                    $displayOrder++;
-                }
+                $em->remove($image);
             }
+        }
+
+        // Flush the database
+        $em->flush();
+    }
+
+    public function getClassName($objectType)
+    {
+        switch ($objectType)
+        {
+            case 'brand':
+                $className = "KAC\\SiteBundle\\Entity\\Brand\\Image";
+                break;
+            case 'department':
+                $className = "KAC\\SiteBundle\\Entity\\Department\\Image";
+                break;
+            case 'product_variant':
+                $className = "KAC\\SiteBundle\\Entity\\Product\\Variant\\Image";
+                break;
+            case 'product':
+            default:
+                $className = "KAC\\SiteBundle\\Entity\\Product\\Image";
+                break;
+        }
+
+        return $className;
+    }
+
+    public function getObject($object)
+    {
+        switch (get_class($object))
+        {
+            case 'KAC\\SiteBundle\\Entity\\Brand\\Image':
+                return $object->getBrand();
+                break;
+            case 'KAC\\SiteBundle\\Entity\\Department\\Image':
+                return $object->getDepartment();
+                break;
+            case 'KAC\\SiteBundle\\Entity\\Product\\Variant\\Image':
+                return $object->getVariant();
+                break;
+            case 'KAC\\SiteBundle\\Entity\\Product\\Image':
+            default:
+                return $object->getProduct();
+                break;
         }
     }
+
 }
