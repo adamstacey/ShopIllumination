b88c278e87c794504038934061286a4ae370be28
Daniel Cannon <daniel@danielcannon.co.uk>
2013-06-12 10:38:10 +0100

    Added basket summary to the header

---
+++ src/KAC/SiteBundle/Controller/BasketController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace KAC\SiteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * @Route("/basket")
+ */
+class BasketController extends Controller
+{
+    public function summaryAction(Request $request)
+    {
+        $basket = $this->get('session')->get('basket');
+
+        return $this->render('KACSiteBundle:Basket:summary.html.twig', array(
+            'basket' => $basket,
+        ));
+    }
+}
