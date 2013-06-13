7903335a1b058532eda8a54efa93df9673699855
Daniel Cannon <daniel@danielcannon.co.uk>
2013-06-13 15:50:29 +0100

    Updated fix routes command to fix brand and department routes

--- src/KAC/SiteBundle/Command/UpdateRoutesCommand.php
+++ src/KAC/SiteBundle/Command/UpdateRoutesCommand.php
@@ -4,6 +4,9 @@ namespace KAC\SiteBundle\Command;
 use Doctrine\ORM\EntityManager;
 use KAC\SiteBundle\Entity\Brand;
 use KAC\SiteBundle\Entity\BrandToDepartment;
+use KAC\SiteBundle\Entity\Product\Routing;
+use KAC\SiteBundle\Entity\Product;
+use KAC\SiteBundle\Entity\Product\Variant;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -26,8 +29,11 @@ class UpdateRoutesCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         /**
-         * @var $brand Brand
+         * @var $department Department
          * @var $departmentJoin BrandToDepartment
+         * @var $brand Brand
+         * @var $product Product
+         * @var $variant Variant
          */
         $em = $this->getContainer()->get('doctrine')->getManager();
         $seo = $this->getContainer()->get('kac_site.manager.seo');
@@ -35,9 +41,61 @@ class UpdateRoutesCommand extends ContainerAwareCommand
         $i = 0;
         $batchSize = 20;
 
-        // TODO: Check that department has at least 1 route
-        // TODO: Check that brand has at least 1 route
-        // TODO: Check that product has at least 1 route
+        // Generate the brand routes
+        // Fetch the brands
+        $brands = $em->getRepository("KAC\\SiteBundle\\Entity\\Brand")->findAll();
+        foreach($brands as $brand)
+        {
+            // Check if a route already exists
+            $routes = $em->getRepository("KAC\\SiteBundle\\Entity\\Brand\\Routing")->findBy(array(
+                'brand' => $brand->getId(),
+            ));
+
+            // If no routes were found create a new route
+            if(count($routes) <= 0)
+            {
+                $route = new Brand\Routing();
+                $route->setBrand($brand);
+                $route->setLocale('en');
+                $route->setUrl($seo->generateUrl($brand->getDescription()->getPageTitle()));
+
+                $em->persist($route);
+
+                $i++;
+            }
+
+            if (($i % $batchSize) === 0) {
+                $em->flush();
+            }
+        }
+
+        // Generate the department routes
+        // Fetch the brands
+        $departments = $em->getRepository("KAC\\SiteBundle\\Entity\\Department")->findAll();
+        foreach($departments as $department)
+        {
+            // Check if a route already exists
+            $routes = $em->getRepository("KAC\\SiteBundle\\Entity\\Department\\Routing")->findBy(array(
+                'department' => $department->getId(),
+            ));
+
+            // If no routes were found create a new route
+            if(count($routes) <= 0)
+            {
+                $route = new Department\Routing();
+                $route->setDepartment($department);
+                $route->setLocale('en');
+                $route->setUrl($seo->createUrl($department->getDescription()->getPageTitle()));
+
+                $em->persist($route);
+
+                $i++;
+            }
+
+            if (($i % $batchSize) === 0) {
+                $em->flush();
+            }
+        }
 
         // Generate the brands_with_departments routes
         // Fetch the brands
@@ -61,7 +119,7 @@ class UpdateRoutesCommand extends ContainerAwareCommand
                     $route->setBrand($brand);
                     $route->setDepartment($department);
                     $route->setLocale('en');
-                    $route->setUrl($seo->generateUrl('brand/'.$brand->getDescription()->getHeader().'/'.$department->getRouting()->getUrl()));
+                    $route->setUrl($seo->generateUrl('brand/'.$brand->getRouting()->getUrl().'/'.$department->getRouting()->getUrl()));
 
                     $em->persist($route);
 
