9fc563eb7ba5d6be57b96e8b70e70fb8a6816950
Adam Stacey <me@adamstacey.co.uk>
2013-06-17 12:42:39 +0100

    Added a new script to handle the new slider instantiations.

---
+++ src/KAC/SiteBundle/Resources/public/js/sliders/sliders.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".slider").rhinoslider({
+        randomOrder: true,
+        autoPlay: true,
+        showBullets: 'always',
+        showControls: 'always'
+    });
+});
