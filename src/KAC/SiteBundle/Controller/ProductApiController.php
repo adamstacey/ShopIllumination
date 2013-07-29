8eb3c05da42e4c1e22380191e263afad097f1bef
Daniel Cannon <daniel@danielcannon.co.uk>
2013-07-29 14:07:22 +0100

    Added API method to update product prices

--- src/KAC/SiteBundle/Controller/ProductApiController.php
+++ src/KAC/SiteBundle/Controller/ProductApiController.php
@@ -14,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use KAC\SiteBundle\Entity\Product;
 use KAC\SiteBundle\Manager\Productmanager;
@@ -68,6 +69,39 @@ class ProductApiController extends Controller
         $em = $this->getDoctrine()->getManager();
 
         $product = $em->getRepository("KAC\SiteBundle\Entity\Product")->find($id);
+
+        $serializer = $this->get('serializer');
+        $data = $serializer->serialize($product, $format);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/'.$format);
+        return $response;
+    }
+
+    /**
+     * @Route("/{id}/price.{format}", name="api_products_post_price", defaults={"format":"json"}, requirements={"format":"json|xml"})
+     * @Method({"POST"})
+     * @Secure({"ROLE_ADMIN"})
+     */
+    public function updatePriceAction(Request $request, $id, $format)
+    {
+        /**
+         * @var $em EntityManager
+         */
+        $em = $this->getDoctrine()->getManager();
+
+        $product = $em->getRepository("KAC\SiteBundle\Entity\Product")->find($id);
+        if(!$product)
+        {
+            throw new NotFoundHttpException();
+        }
+        if(!$request->request->get('price') && count($product->getVariants()) === 1)
+        {
+            throw new HttpException(403);
+        }
+        $product->getVariant()->getPrice()->setListPrice(floatval($request->request->get('price')));
+        $em->persist($product);
+        $em->flush();
 
         $serializer = $this->get('serializer');
         $data = $serializer->serialize($product, $format);
