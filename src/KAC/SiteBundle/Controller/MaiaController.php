6dc1adeee1c3daaa04b9649486d1cbed8783b952
Adam Stacey <me@adamstacey.co.uk>
2013-07-31 10:20:34 +0100

    Added a Maia controller and templates.

---
+++ src/KAC/SiteBundle/Controller/MaiaController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace KAC\SiteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class MaiaController extends Controller
+{
+
+    /**
+     * @Route("/maia.html", name="maia")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-ammonite.html", name="maia_ammonite")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaAmmoniteAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-calcite.html", name="maia_calcite")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaCalciteAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-fossil.html", name="maia_fossil")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaFossilAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-meteorite.html", name="maia_meteorite")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaMeteoriteAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-pearlstone.html", name="maia_pearlstone")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaPearlstoneAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-sandstone.html", name="maia_sandstone")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaSandstoneAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-cappucino.html", name="maia_cappuccino")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaCappuccinoAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-cristallo.html", name="maia_cristallo")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaCristalloAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-galaxy.html", name="maia_galaxy")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaGalaxyAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-iceberg.html", name="maia_iceberg")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaIcebergAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-latte.html", name="maia_latte")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaLatteAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-lava.html", name="maia_lava")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaLavaAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-mocha.html", name="maia_mocha")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaMochaAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-vanilla.html", name="maia_vanilla")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaVanillaAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/maia-vulcano.html", name="maia_vulcano")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function maiaVulcanoAction(Request $request)
+    {
+
+    }
+}
