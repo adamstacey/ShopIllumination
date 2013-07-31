34914d51e616539806a724a32e45a57d24aee82e
Adam Stacey <me@adamstacey.co.uk>
2013-07-31 14:43:38 +0100

    Added function to handle the adding of non products to the basket.

--- src/KAC/SiteBundle/Resources/public/js/products/products.js
+++ src/KAC/SiteBundle/Resources/public/js/products/products.js
@@ -5,12 +5,56 @@ $(document).ready(function() {
 
         $("#ajax-loading").show();
         $.ajax({
-            type: "GET",
+            type: "POST",
             dataType: "json",
             url: $el.attr("data-basket-url"),
             data: {
                 productId: $el.attr("data-product-id"),
                 variantId: $el.attr("data-variant-id"),
+                quantity: $quantityInput.val()
+            },
+            error: function(a, b, c) {
+                console.log('error', a, b,c );
+                $("#message-error-text").html('Sorry, there was a problem adding the item to your basket. Please try again.');
+                $("#message-error").fadeIn(function() {
+                    $("html, body").animate({scrollTop: $("#message-error").offset().top - 15},'slow');
+                    $("#ajax-loading").hide();
+                });
+            },
+            success: function(a, b, c) {
+                console.log('success', a, b,c );
+                updateBasketSummary();
+                $("html, body").animate({scrollTop: 0},'slow', function() {
+                    $('.dropdown-summary').show();
+                    $('#basket-summary').addClass('expanded');
+                    setTimeout(function() {
+                        $('.dropdown-summary').hide();
+                        $('#basket-summary').removeClass('expanded');
+                    }, 5000);
+                });
+            }
+        });
+    });
+
+    $(".actionAddNonProductToBasket").on('click', function() {
+        var $quantityInput = $(this).parent().find("input.quantity"),
+            $el = $(this).parent();
+
+        $("#ajax-loading").show();
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: $el.attr("data-basket-url"),
+            data: {
+                productId: $el.attr("data-id"),
+                variantId: $el.attr("data-id"),
+                deliveryBand: $el.attr("data-delivery-band"),
+                url: $el.attr("data-url"),
+                unitCost: $el.attr("data-unit-cost"),
+                productCode: $el.attr("data-product-code"),
+                header: $el.attr("data-header"),
+                brand: $el.attr("data-brand"),
+                description: $el.attr("data-description"),
                 quantity: $quantityInput.val()
             },
             error: function(a, b, c) {
