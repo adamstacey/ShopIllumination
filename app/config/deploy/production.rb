9b9908bece1976508d336f2b9459c9c4b8f4dfc3
Daniel Cannon <daniel@danielcannon.co.uk>
2013-08-05 20:51:26 +0100

    Added config for the production capifony solr commands

--- app/config/deploy/production.rb
+++ app/config/deploy/production.rb
@@ -3,4 +3,7 @@ set :user, "root"
 set :deploy_to, "/home/sites/#{domain}"
 ssh_options[:port] = 2020
 
+set :solr_path, "http://kitchenappliancecentre.co.uk:8080/solr"
+set :solr_dir, "/var/solr"
+
 server "server.kitchenappliancecentre.co.uk", :app, :web, :db, :primary => true
