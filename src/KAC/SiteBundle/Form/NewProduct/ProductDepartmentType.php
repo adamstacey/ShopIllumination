f025d0f8bfdbd6b2be7de48a9727b18ba0ddd0e8
Adam Stacey <me@adamstacey.co.uk>
2013-08-16 17:23:02 +0100

    Added new product department type for new product.

--- src/KAC/SiteBundle/Form/NewProduct/ProductDepartmentType.php
+++ src/KAC/SiteBundle/Form/NewProduct/ProductDepartmentType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace KAC\SiteBundle\Form\Product;
+namespace KAC\SiteBundle\Form\NewProduct;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
