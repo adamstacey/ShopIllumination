db1aff0dc10e63f04f9bd40225745ef11196dd67
Adam Stacey <me@adamstacey.co.uk>
2013-08-22 10:55:15 +0100

    Updated the database migration file to update the routing and redirects.

--- app/DoctrineMigrations/update_3.sql
+++ app/DoctrineMigrations/update_3.sql
@@ -1,8 +1,12 @@
-
 SET FOREIGN_KEY_CHECKS = 0;
 ALTER TABLE  `order_products` DROP FOREIGN KEY  `FK_5242B8EB3B69A9AF` ,
 ADD FOREIGN KEY (  `variant_id` ) REFERENCES  `kac`.`product_variants` (
 
     `id`
 ) ON DELETE SET NULL ON UPDATE RESTRICT ;
+
+# 21/08/2013
+UPDATE routing SET object_type = 'product_variant' WHERE object_type = 'product';
+UPDATE redirects SET object_type = 'product_variant' WHERE object_type = 'product';
+
 SET FOREIGN_KEY_CHECKS = 1;
